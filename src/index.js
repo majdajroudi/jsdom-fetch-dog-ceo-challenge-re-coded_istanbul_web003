9b84e8aa502bdf4264af11cadbdcc064a564251b
Majd Ajroudi <majd.ajroudi@gmail.com>
2020-10-26 09:03:05 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl ="https://dog.ceo/api/breeds/image/random/4";
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+function renderImages(images) {
+    const imgContainer = document.getElementById("dog-image-container")
+    images.message.forEach(img => {
+        let imgElement = document.createElement("img");
+        imgElement.src = img;
+        imgContainer.appendChild(imgElement)
+    })
+}
+
+function colorChanger(element) {
+    element.style.color = "red"
+}
+
+function renderBreeds(images){
+    const dogBreeds = document.querySelector("#dog-breeds")
+    Object.keys(images.message).forEach(breed => {
+        if (images.message[breed].length !== 0){
+            images.message[breed].forEach(subBreed => {
+                let breedElement = document.createElement("li");
+                breedElement.innerText = subBreed;
+                breedElement.addEventListener("click", (e) => colorChanger(e.target))
+                dogBreeds.appendChild(breedElement)
+            })
+        }
+    })
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const selectDropdown = document.querySelector("#breed-dropdown");
+    selectDropdown.addEventListener("change", console.log(selectDropdown.value))
+    fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(json => renderImages(json))
+
+    fetch(breedUrl)
+    .then(resp => resp.json())
+    .then(json => renderBreeds(json))
+})
